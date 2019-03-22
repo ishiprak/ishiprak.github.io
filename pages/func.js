b3aa42f85ca2f7bea6971872028a305a48db026b
Shivam Prakash <shivamprakash113@gmail.com>
2019-03-22 17:10:23 +0530

    Add files via upload

---
+++ pages/func.js
@@ -0,0 +1,18 @@
+AOS.init();
+
+function wraps(){
+  var elem = document.getElementById("wrapper");
+  elem.classList.add("loaded");
+}
+
+/*$(window).load(function(){
+  $('#wrapper').addClass('loaded');
+})
+
+$('.more-info').click(function(){
+  $("#card").toggleClass('flip');
+  $('#arrow').remove();
+});
+$('#background').click(function(){
+  $('#card').removeClass('flip');
+})*/
